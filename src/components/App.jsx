85a5019140d79a985e298eceb4f99e50a49d337c
İsmail Sezer Bullalıoğlu <bullalioglu@gmail.com>
2022-01-29 01:38:31 +0300

    Map data to dynamically create Note components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,21 @@ import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 
+// Import Constants
+import notes from "../notes";
+
+const createNotes = notes.map(note =>
+   <Note
+       key={note.key}
+       title={note.title}
+       content={note.content}
+   />
+);
+
 const App = () => (
     <div>
         <Header />
-        <Note />
+        {createNotes}
         <Footer />
     </div>
 );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Note = () => (
+const Note = (props) => (
     <div className="note">
-        <h1>Note Title</h1>
-        <p>Note content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 );
 
