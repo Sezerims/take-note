8aec33a34939dd40be06ac96d18e6dee9451d06b
İsmail Sezer Bullalıoğlu <bullalioglu@gmail.com>
2022-01-29 03:25:17 +0300

    Add a deleted note back in

--- src/notes.js
+++ src/notes.js
@@ -16,6 +16,12 @@ const notes = [
         title: "Arrays",
         content:
             "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+    },
+    {
+        key: 4,
+        title: "Hardware vs. Software",
+        content:
+            "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
     }
 ];
 
